752baf366a91238c412e3502cab5d4c566c3666c
Tobias Jordan <tjjordan@githb.nomail>
2022-11-08 16:43:09 +0100

    ran format script

--- scripts/plot_file_io_benchmark_results.py
+++ scripts/plot_file_io_benchmark_results.py
@@ -10,31 +10,26 @@ import sys
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#set plot styles
-sns.set_style('whitegrid')
-plt.style.use('ggplot')
+# set plot styles
+sns.set_style("whitegrid")
+plt.style.use("ggplot")
 
 if len(sys.argv) != 2:
     sys.exit("Usage: " + sys.argv[0] + " benchmark.csv")
 
 with open(sys.argv[1]) as csv_file:
     df = pd.read_csv(csv_file)
-    df[['fixture', 'type', 'filesize_mb']] = df['name'].str.split('/', 2, expand=True)
-    df['filesize_mb'] = pd.to_numeric(df['filesize_mb'])
-    df['real_time_sec'] = df['real_time'] / 1000000000
-    df['mb_per_sec'] = df['filesize_mb'] / df['real_time_sec']
+    df[["fixture", "type", "filesize_mb"]] = df["name"].str.split("/", 2, expand=True)
+    df["filesize_mb"] = pd.to_numeric(df["filesize_mb"])
+    df["real_time_sec"] = df["real_time"] / 1000000000
+    df["mb_per_sec"] = df["filesize_mb"] / df["real_time_sec"]
 
-benchmark_results = sns.lineplot(data=df,
-                                 x='filesize_mb',
-                                 y='mb_per_sec',
-                                 hue='type',
-                                 marker='o')
+benchmark_results = sns.lineplot(data=df, x="filesize_mb", y="mb_per_sec", hue="type", marker="o")
 
 # make file size axis logarithmic
-benchmark_results.set(xscale='log', xticks=df['filesize_mb'], xticklabels=df['filesize_mb'])
+benchmark_results.set(xscale="log", xticks=df["filesize_mb"], xticklabels=df["filesize_mb"])
 
-benchmark_results.set(xlabel ='Filesize in MB', ylabel = 'MB/s', title ='Different I/O method speed dependent on filesize')
+benchmark_results.set(xlabel="Filesize in MB", ylabel="MB/s", title="Different I/O method speed dependent on filesize")
 
-plt.legend(title='I/O Type')
+plt.legend(title="I/O Type")
 plt.show()
-
