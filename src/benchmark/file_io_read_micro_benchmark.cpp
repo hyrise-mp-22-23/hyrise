b2c8b420dbff87b28d3649fc1d692501c0c18e2b
Sandro Speh <sandrospeh@googlemail.com>
2022-11-29 16:02:20 +0100

    wip threaded read sequential

--- src/benchmark/file_io_read_micro_benchmark.cpp
+++ src/benchmark/file_io_read_micro_benchmark.cpp
@@ -9,6 +9,7 @@
 #include <numeric>
 #include <ostream>
 #include <random>
+#include <thread>
 #include "micro_benchmark_basic_fixture.hpp"
 
 namespace hyrise {
@@ -135,6 +136,72 @@ BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, READ_NON_ATOMIC_RANDOM)(benc
   }
 
   close(fd);
+}
+
+void read_data_using_read(const size_t from, const size_t to, int32_t fd, uint32_t* read_data_start){
+  const auto uint32_t_size = ssize_t{sizeof(uint32_t)};
+  const auto bytes_to_read = static_cast<ssize_t>(uint32_t_size * (to - from));
+  lseek(fd, from, SEEK_SET);
+  if (read(fd, read_data_start + from, bytes_to_read) != bytes_to_read) {
+    Fail("read error: " + strerror(errno));
+  }
+}
+
+BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, READ_NON_ATOMIC_SEQUENTIAL_THREADED)(benchmark::State& state) {
+  auto thread_count = size_t{10}; // TODO replace with state.range(1)
+
+  auto filedescriptors = std::vector<int32_t>();
+  for (auto i = size_t{0}; i < thread_count; i++) {
+    auto fd = int32_t{};
+    if ((fd = open("file.txt", O_RDONLY)) < 0) {
+      std::cout << "open error " << errno << std::endl;
+    }
+    // TODO index write instead of pushback
+    filedescriptors.push_back(fd);
+  }
+
+  const auto NUMBER_OF_BYTES = uint32_t{static_cast<uint32_t>(state.range(0) * MB)};
+  const auto uint32_t_size = ssize_t{sizeof(uint32_t)};
+  const auto read_data_size = NUMBER_OF_BYTES / uint32_t_size;
+  // const auto max_read_data_size = static_cast<size_t>(read_data_size);
+
+  auto threads = std::vector<std::thread>(thread_count);
+  auto batch_size = NUMBER_OF_BYTES/thread_count;
+
+  for (auto _ : state) {
+    state.PauseTiming();
+
+    micro_benchmark_clear_disk_cache();
+    auto read_data = std::vector<uint32_t>{};
+    read_data.resize(read_data_size);
+    auto* read_data_start = std::data(read_data);
+
+    state.ResumeTiming();
+
+    for (auto i = size_t{0}; i < thread_count; i++){
+      size_t from = batch_size * i;
+      size_t to = from + batch_size - 1;
+      std::cout << "Thread " << i << ": " << from << " - " << to << std::endl;
+      threads.push_back(std::thread(read_data_using_read, from, to, filedescriptors[i], read_data_start));
+    }
+
+    // TODO: Discuss if timing is set to start here
+    // state.ResumeTiming();
+    for (auto i = size_t{0}; i < thread_count; i++) {
+      // Explain: Blocks the current thread until the thread identified by *this finishes its execution
+      threads[i].join();
+    }
+    state.PauseTiming();
+    threads.clear();
+
+    const auto sum = std::accumulate(read_data.begin(), read_data.end(), uint64_t{0});
+    Assert(control_sum == sum, "Sanity check failed: Not the same result");
+    state.ResumeTiming();
+  }
+
+  for (auto i = size_t{0}; i < thread_count; i++) {
+    close(filedescriptors[i]);
+  }
 }
 
 BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, PREAD_ATOMIC_SEQUENTIAL)(benchmark::State& state) {
@@ -433,19 +500,20 @@ BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, IN_MEMORY_READ_RANDOM)(bench
 }
 
 // Arguments are file size in MB
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, READ_NON_ATOMIC_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, READ_NON_ATOMIC_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
+//BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, READ_NON_ATOMIC_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
+//BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, READ_NON_ATOMIC_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
+BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, READ_NON_ATOMIC_SEQUENTIAL_THREADED)->Arg(1);//0)->Arg(100)->Arg(1000);
 
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, PREAD_ATOMIC_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, PREAD_ATOMIC_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
+//BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, PREAD_ATOMIC_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
+//BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, PREAD_ATOMIC_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
 
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, IN_MEMORY_READ_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, IN_MEMORY_READ_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
+//BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, IN_MEMORY_READ_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
+//BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, IN_MEMORY_READ_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
 
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
+//BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
+//BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
 
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
+//BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
+//BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
 
 }  // namespace hyrise
