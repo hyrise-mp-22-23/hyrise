46fc7210fbdb99756feb7577989992eba9a2a5f2
Sandro Speh <sandrospeh@googlemail.com>
2022-12-04 13:42:10 +0100

    Split up into mmap and system calls with small refactoring - Setup mmap filestructure described in c5dc89e63af81540efd2af40a937d63da181c31a - removal of vector_element_count variable - renaming of read_data_size to NUMBERS_OF_ELEMENTS

--- src/benchmark/file_io_read_micro_benchmark.cpp
+++ src/benchmark/file_io_read_micro_benchmark.cpp
@@ -8,46 +8,9 @@
 #include <numeric>
 #include <random>
 #include "micro_benchmark_basic_fixture.hpp"
+#include "file_io_read_micro_benchmark.hpp"
 
 namespace hyrise {
-
-const auto MB = uint32_t{1'000'000};
-
-class FileIOMicroReadBenchmarkFixture : public MicroBenchmarkBasicFixture {
- public:
-  void SetUp(::benchmark::State& state) override {
-    NUMBER_OF_BYTES = uint32_t{static_cast<uint32_t>(state.range(0) * MB)};
-    read_data_size = NUMBER_OF_BYTES / uint32_t_size;
-    auto BUFFER_SIZE_MB = state.range(0);
-
-    // each int32_t contains four bytes
-    vector_element_count = (BUFFER_SIZE_MB * MB) / sizeof(uint32_t);
-    numbers = generate_random_numbers(vector_element_count);
-    control_sum = std::accumulate(numbers.begin(), numbers.end(), uint64_t{0});
-
-    auto fd = int32_t{};
-    Assert(((fd = creat(filename, O_WRONLY)) >= 1), fail_and_close_file(fd, "Create error: ", errno));
-    chmod(filename, S_IRWXU);  // enables owner to rwx file
-    Assert((write(fd, std::data(numbers), BUFFER_SIZE_MB * MB) == BUFFER_SIZE_MB * MB),
-           fail_and_close_file(fd, "Write error: ", errno));
-
-    close(fd);
-  }
-
-  void TearDown(::benchmark::State& /*state*/) override {
-    Assert(std::remove(filename) == 0, "Remove error: " + std::strerror(errno));
-  }
-
- protected:
-  const char* filename = "file.txt";  //const char* needed for C-System Calls
-  const ssize_t uint32_t_size = ssize_t{sizeof(uint32_t)};
-  std::vector<uint32_t> data_to_write;
-  uint64_t control_sum = uint64_t{0};
-  uint32_t NUMBER_OF_BYTES = uint32_t{0};
-  uint32_t read_data_size = NUMBER_OF_BYTES / uint32_t_size;
-  std::vector<uint32_t> numbers = std::vector<uint32_t>{};
-  uint32_t vector_element_count;
-};
 
 BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, READ_NON_ATOMIC_SEQUENTIAL)(benchmark::State& state) {  // open file
   auto fd = int32_t{};
@@ -58,12 +21,12 @@ BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, READ_NON_ATOMIC_SEQUENTIAL)(
 
     micro_benchmark_clear_disk_cache();
     auto read_data = std::vector<uint32_t>{};
-    read_data.resize(read_data_size);
+    read_data.resize(NUMBER_OF_ELEMENTS);
 
     state.ResumeTiming();
 
     // TODO Remove looping
-    for (auto index = size_t{0}; index < read_data_size; ++index) {
+    for (auto index = size_t{0}; index < NUMBER_OF_ELEMENTS; ++index) {
       lseek(fd, uint32_t_size * index, SEEK_SET);
       Assert((read(fd, std::data(read_data) + index, uint32_t_size) == uint32_t_size),
              fail_and_close_file(fd, "Read error: ", errno));
@@ -88,16 +51,16 @@ BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, READ_NON_ATOMIC_RANDOM)(benc
     state.PauseTiming();
 
     micro_benchmark_clear_disk_cache();
-    const auto random_indices = generate_random_indexes(vector_element_count);
+    const auto random_indices = generate_random_indexes(NUMBER_OF_ELEMENTS);
     auto read_data = std::vector<uint32_t>{};
-    read_data.resize(read_data_size);
+    read_data.resize(NUMBER_OF_ELEMENTS);
     auto* read_data_start = std::data(read_data);
 
     state.ResumeTiming();
 
     lseek(fd, 0, SEEK_SET);
     // TODO Remove lseek loop
-    for (auto index = size_t{0}; index < read_data_size; ++index) {
+    for (auto index = size_t{0}; index < NUMBER_OF_ELEMENTS; ++index) {
       lseek(fd, uint32_t_size * random_indices[index], SEEK_SET);
       // TODO do we need read data start?
       Assert((read(fd, read_data_start + index, uint32_t_size) == uint32_t_size),
@@ -124,13 +87,13 @@ BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, PREAD_ATOMIC_SEQUENTIAL)(ben
 
     micro_benchmark_clear_disk_cache();
     auto read_data = std::vector<uint32_t>{};
-    read_data.resize(read_data_size);
+    read_data.resize(NUMBER_OF_ELEMENTS);
     auto* read_data_start = std::data(read_data);
 
     state.ResumeTiming();
 
     // TODO remov loop
-    for (auto index = size_t{0}; index < read_data_size; ++index) {
+    for (auto index = size_t{0}; index < NUMBER_OF_ELEMENTS; ++index) {
       // TODO remove read data start
       Assert((pread(fd, read_data_start + index, uint32_t_size, uint32_t_size * index) == uint32_t_size),
              fail_and_close_file(fd, "Read error: ", errno));
@@ -146,139 +109,6 @@ BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, PREAD_ATOMIC_SEQUENTIAL)(ben
   close(fd);
 }
 
-BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_RANDOM)(benchmark::State& state) {
-  auto fd = int32_t{};
-  Assert(((fd = open(filename, O_RDONLY)) >= 0), fail_and_close_file(fd, "Open error: ", errno));
-
-  for (auto _ : state) {
-    state.PauseTiming();
-    const auto random_indices = generate_random_indexes(vector_element_count);
-    micro_benchmark_clear_disk_cache();
-    state.ResumeTiming();
-
-    // Getting the mapping to memory.
-    const auto OFFSET = off_t{0};
-
-    auto* map = reinterpret_cast<int32_t*>(mmap(NULL, NUMBER_OF_BYTES, PROT_READ, MAP_PRIVATE, fd, OFFSET));
-    Assert((map != MAP_FAILED), fail_and_close_file(fd, "Mapping failed: ", errno));
-
-    madvise(map, NUMBER_OF_BYTES, MADV_RANDOM);
-
-    auto sum = uint64_t{0};
-    for (size_t index = 0; index < vector_element_count; ++index) {
-      sum += map[random_indices[index]];
-    }
-
-    state.PauseTiming();
-    Assert(control_sum == sum, "Sanity check failed: Not the same result");
-    state.ResumeTiming();
-
-    // Remove memory mapping after job is done.
-    Assert((munmap(map, NUMBER_OF_BYTES) == 0), fail_and_close_file(fd, "Unmapping failed: ", errno));
-  }
-  close(fd);
-}
-
-BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_SEQUENTIAL)(benchmark::State& state) {
-  auto fd = int32_t{};
-  Assert(((fd = open(filename, O_RDONLY)) >= 0), fail_and_close_file(fd, "Open error: ", errno));
-
-  for (auto _ : state) {
-    state.PauseTiming();
-    micro_benchmark_clear_disk_cache();
-    state.ResumeTiming();
-
-    // Getting the mapping to memory.
-    const auto OFFSET = off_t{0};
-
-    auto* map = reinterpret_cast<int32_t*>(mmap(NULL, NUMBER_OF_BYTES, PROT_READ, MAP_PRIVATE, fd, OFFSET));
-    Assert((map != MAP_FAILED), fail_and_close_file(fd, "Mapping Failed: ", errno));
-
-    madvise(map, NUMBER_OF_BYTES, MADV_SEQUENTIAL);
-
-    auto sum = uint64_t{0};
-    for (auto index = size_t{0}; index < vector_element_count; ++index) {
-      sum += map[index];
-    }
-
-    state.PauseTiming();
-    Assert(control_sum == sum, "Sanity check failed: Not the same result");
-    state.ResumeTiming();
-
-    // Remove memory mapping after job is done.
-    Assert((munmap(map, NUMBER_OF_BYTES) == 0), fail_and_close_file(fd, "Unmapping failed: ", errno));
-  }
-
-  close(fd);
-}
-
-BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_RANDOM)(benchmark::State& state) {
-  auto fd = int32_t{};
-  Assert(((fd = open(filename, O_RDONLY)) >= 0), fail_and_close_file(fd, "Open error: ", errno));
-
-  for (auto _ : state) {
-    state.PauseTiming();
-    const auto random_indices = generate_random_indexes(vector_element_count);
-    micro_benchmark_clear_disk_cache();
-    state.ResumeTiming();
-
-    // Getting the mapping to memory.
-    const auto OFFSET = off_t{0};
-
-    auto* map = reinterpret_cast<int32_t*>(mmap(NULL, NUMBER_OF_BYTES, PROT_READ, MAP_SHARED, fd, OFFSET));
-    Assert((map != MAP_FAILED), fail_and_close_file(fd, "Mapping failed: ", errno));
-
-    madvise(map, NUMBER_OF_BYTES, MADV_RANDOM);
-
-    auto sum = uint64_t{0};
-    for (auto index = size_t{0}; index < vector_element_count; ++index) {
-      sum += map[random_indices[index]];
-    }
-
-    state.PauseTiming();
-    Assert(control_sum == sum, "Sanity check failed: Not the same result");
-    state.ResumeTiming();
-
-    // Remove memory mapping after job is done.
-    Assert((munmap(map, NUMBER_OF_BYTES) == 0), fail_and_close_file(fd, "Unmapping failed: ", errno));
-  }
-
-  close(fd);
-}
-
-BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_SEQUENTIAL)(benchmark::State& state) {
-  auto fd = int32_t{};
-  Assert(((fd = open(filename, O_RDONLY)) >= 0), fail_and_close_file(fd, "Open error: ", errno));
-
-  for (auto _ : state) {
-    state.PauseTiming();
-    micro_benchmark_clear_disk_cache();
-    state.ResumeTiming();
-
-    // Getting the mapping to memory.
-    const auto OFFSET = off_t{0};
-
-    auto* map = reinterpret_cast<int32_t*>(mmap(NULL, NUMBER_OF_BYTES, PROT_READ, MAP_SHARED, fd, OFFSET));
-    Assert((map != MAP_FAILED), fail_and_close_file(fd, "Mapping failed: ", errno));
-
-    madvise(map, NUMBER_OF_BYTES, MADV_SEQUENTIAL);
-
-    auto sum = uint64_t{0};
-    for (size_t index = 0; index < vector_element_count; ++index) {
-      sum += map[index];
-    }
-
-    state.PauseTiming();
-    Assert(control_sum == sum, "Sanity check failed: Not the same result");
-    state.ResumeTiming();
-
-    // Remove memory mapping after job is done.
-    Assert((munmap(map, NUMBER_OF_BYTES) == 0), fail_and_close_file(fd, "Unmapping failed: ", errno));
-  }
-
-  close(fd);
-}
-
 BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, PREAD_ATOMIC_RANDOM)(benchmark::State& state) {
   auto fd = int32_t{};
   Assert(((fd = open(filename, O_RDONLY)) >= 0), fail_and_close_file(fd, "Open error: ", errno));
@@ -286,15 +116,15 @@ BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, PREAD_ATOMIC_RANDOM)(benchma
   for (auto _ : state) {
     state.PauseTiming();
     micro_benchmark_clear_disk_cache();
-    const auto random_indices = generate_random_indexes(vector_element_count);
+    const auto random_indices = generate_random_indexes(NUMBER_OF_ELEMENTS);
     auto read_data = std::vector<uint32_t>{};
-    read_data.resize(read_data_size);
+    read_data.resize(NUMBER_OF_ELEMENTS);
     auto* read_data_start = std::data(read_data);
 
     state.ResumeTiming();
 
     // TODO Remove looping
-    for (auto index = size_t{0}; index < read_data_size; ++index) {
+    for (auto index = size_t{0}; index < NUMBER_OF_ELEMENTS; ++index) {
       Assert(
           (pread(fd, read_data_start + index, uint32_t_size, uint32_t_size * random_indices[index]) == uint32_t_size),
           fail_and_close_file(fd, "Read error: ", errno));
@@ -315,11 +145,11 @@ BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, IN_MEMORY_READ_SEQUENTIAL)(b
   for (auto _ : state) {
     state.PauseTiming();
     auto read_data = std::vector<uint32_t>{};
-    read_data.resize(read_data_size);
+    read_data.resize(NUMBER_OF_ELEMENTS);
 
     state.ResumeTiming();
 
-    for (auto index = size_t{0}; index < read_data_size; ++index) {
+    for (auto index = size_t{0}; index < NUMBER_OF_ELEMENTS; ++index) {
       read_data[index] = numbers[index];
     }
 
@@ -336,12 +166,12 @@ BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, IN_MEMORY_READ_SEQUENTIAL)(b
 BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, IN_MEMORY_READ_RANDOM)(benchmark::State& state) {
   for (auto _ : state) {
     state.PauseTiming();
-    const auto random_indices = generate_random_indexes(vector_element_count);
+    const auto random_indices = generate_random_indexes(NUMBER_OF_ELEMENTS);
     auto read_data = std::vector<uint32_t>{};
-    read_data.resize(read_data_size);
+    read_data.resize(NUMBER_OF_ELEMENTS);
     state.ResumeTiming();
 
-    for (auto index = size_t{0}; index < read_data_size; ++index) {
+    for (auto index = size_t{0}; index < NUMBER_OF_ELEMENTS; ++index) {
       read_data[index] = numbers[random_indices[index]];
     }
 
@@ -364,11 +194,5 @@ BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, PREAD_ATOMIC_RANDOM)->Arg(
 
 BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, IN_MEMORY_READ_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
 BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, IN_MEMORY_READ_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
-
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
-
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
 
 }  // namespace hyrise
---
+++ src/benchmark/file_io_read_micro_benchmark.hpp
@@ -0,0 +1,42 @@
+#include "micro_benchmark_basic_fixture.hpp"
+
+#include <fcntl.h>
+#include <numeric>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+namespace hyrise {
+
+const auto MB = uint32_t{1'000'000};
+
+class FileIOMicroReadBenchmarkFixture : public MicroBenchmarkBasicFixture {
+ public:
+  void SetUp(::benchmark::State& state) override {
+    NUMBER_OF_BYTES = state.range(0) * MB;
+    NUMBER_OF_ELEMENTS = NUMBER_OF_BYTES / uint32_t_size;
+
+    // each int32_t contains four bytes
+    numbers = generate_random_numbers(NUMBER_OF_ELEMENTS);
+    control_sum = std::accumulate(numbers.begin(), numbers.end(), uint64_t{0});
+
+    auto fd = int32_t{};
+    Assert(((fd = creat(filename, O_WRONLY)) >= 1), fail_and_close_file(fd, "Create error: ", errno));
+    chmod(filename, S_IRWXU);  // enables owner to rwx file
+    Assert((write(fd, std::data(numbers), NUMBER_OF_BYTES) == NUMBER_OF_BYTES),
+           fail_and_close_file(fd, "Write error: ", errno));
+    close(fd);
+  }
+
+  void TearDown(::benchmark::State& /*state*/) override {
+    Assert(std::remove(filename) == 0, "Remove error: " + std::strerror(errno));
+  }
+
+ protected:
+  const char* filename = "file.txt";  //const char* needed for C-System Calls
+  const ssize_t uint32_t_size = ssize_t{sizeof(uint32_t)};
+  uint64_t control_sum = uint64_t{0};
+  uint32_t NUMBER_OF_BYTES = uint32_t{0};
+  uint32_t NUMBER_OF_ELEMENTS = uint32_t{0};
+  std::vector<uint32_t> numbers = std::vector<uint32_t>{};
+};
+}
