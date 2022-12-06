d144a5d8562453eb1a169384a222457988719167
Sandro Speh <sandrospeh@googlemail.com>
2022-12-06 16:23:40 +0100

    PR feedback: const and formatting

--- src/benchmark/file_io_read_mmap_benchmark.cpp
+++ src/benchmark/file_io_read_mmap_benchmark.cpp
@@ -17,8 +17,8 @@ void read_mmap_chunk_random(const size_t from, const size_t to, const int32_t* m
   }
 }
 
-void FileIOMicroReadBenchmarkFixture::mmap_read_single_threaded(benchmark::State& state, int mmap_mode_flag,
-                                                                int access_order) {
+void FileIOMicroReadBenchmarkFixture::mmap_read_single_threaded(benchmark::State state, const int mmap_mode_flag,
+                                                                const int access_order) {
   auto fd = int32_t{};
   Assert(((fd = open(filename, O_RDONLY)) >= 0), fail_and_close_file(fd, "Open error: ", errno));
 
@@ -54,13 +54,13 @@ void FileIOMicroReadBenchmarkFixture::mmap_read_single_threaded(benchmark::State
   close(fd);
 }
 
-void FileIOMicroReadBenchmarkFixture::mmap_read_multi_threaded(benchmark::State& state, int mmap_mode_flag,
-                                                               uint16_t thread_count, int access_order) {
+void FileIOMicroReadBenchmarkFixture::mmap_read_multi_threaded(benchmark::State& state, const int mmap_mode_flag,
+                                                               const uint16_t thread_count, const int access_order) {
   auto fd = int32_t{};
   Assert(((fd = open(filename, O_RDONLY)) >= 0), fail_and_close_file(fd, "Open error: ", errno));
 
   auto threads = std::vector<std::thread>(thread_count);
-  auto batch_size = static_cast<uint64_t>(std::ceil(static_cast<float>(NUMBER_OF_ELEMENTS) / thread_count));
+  const auto batch_size = static_cast<uint64_t>(std::ceil(static_cast<float>(NUMBER_OF_ELEMENTS) / thread_count));
 
   for (auto _ : state) {
     state.PauseTiming();
@@ -81,28 +81,28 @@ void FileIOMicroReadBenchmarkFixture::mmap_read_multi_threaded(benchmark::State&
 
       madvise(map, NUMBER_OF_BYTES, MADV_RANDOM);
 
-      for (auto i = size_t{0}; i < thread_count; i++) {
-        auto from = batch_size * i;
-        auto to = std::min(from + batch_size, uint64_t{NUMBER_OF_ELEMENTS});
+      for (auto i = size_t{0}; i < thread_count; ++i) {
+        const auto from = batch_size * i;
+        const auto to = std::min(from + batch_size, uint64_t{NUMBER_OF_ELEMENTS});
         // std::ref fix from https://stackoverflow.com/a/73642536
         threads[i] = std::thread(read_mmap_chunk_random, from, to, map, std::ref(sums[i]), random_indexes);
       }
     } else {
       madvise(map, NUMBER_OF_BYTES, MADV_SEQUENTIAL);
 
-      for (auto i = size_t{0}; i < thread_count; i++) {
-        auto from = batch_size * i;
-        auto to = std::min(from + batch_size, uint64_t{NUMBER_OF_ELEMENTS});
+      for (auto i = size_t{0}; i < thread_count; ++i) {
+        const auto from = batch_size * i;
+        const auto to = std::min(from + batch_size, uint64_t{NUMBER_OF_ELEMENTS});
         // std::ref fix from https://stackoverflow.com/a/73642536
         threads[i] = std::thread(read_mmap_chunk_sequential, from, to, map, std::ref(sums[i]));
       }
     }
-    for (auto i = size_t{0}; i < thread_count; i++) {
+    for (auto i = size_t{0}; i < thread_count; ++i) {
       // Blocks the current thread until the thread identified by *this finishes its execution
       threads[i].join();
     }
     state.PauseTiming();
-    auto total_sum = std::accumulate(sums.begin(), sums.end(), uint64_t{0});
+    const auto total_sum = std::accumulate(sums.begin(), sums.end(), uint64_t{0});
 
     Assert(control_sum == total_sum, "Sanity check failed: Not the same result");
     state.ResumeTiming();
@@ -119,7 +119,7 @@ void FileIOMicroReadBenchmarkFixture::mmap_read_multi_threaded(benchmark::State&
 }
 
 BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_RANDOM)(benchmark::State& state) {
-  auto thread_count = static_cast<uint16_t>(state.range(1));
+  const auto thread_count = static_cast<uint16_t>(state.range(1));
   if (thread_count == 1) {
     mmap_read_single_threaded(state, PRIVATE, RANDOM);
   } else {
@@ -128,7 +128,7 @@ BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_RAND
 }
 
 BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_SEQUENTIAL)(benchmark::State& state) {
-  auto thread_count = static_cast<uint16_t>(state.range(1));
+  const auto thread_count = static_cast<uint16_t>(state.range(1));
   if (thread_count == 1) {
     mmap_read_single_threaded(state, PRIVATE, SEQUENTIAL);
   } else {
@@ -137,7 +137,7 @@ BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_SEQU
 }
 
 BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_RANDOM)(benchmark::State& state) {
-  auto thread_count = static_cast<uint16_t>(state.range(1));
+  const auto thread_count = static_cast<uint16_t>(state.range(1));
   if (thread_count == 1) {
     mmap_read_single_threaded(state, SHARED, RANDOM);
   } else {
@@ -146,7 +146,7 @@ BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_RANDO
 }
 
 BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_SEQUENTIAL)(benchmark::State& state) {
-  auto thread_count = static_cast<uint16_t>(state.range(1));
+  const auto thread_count = static_cast<uint16_t>(state.range(1));
   if (thread_count == 1) {
     mmap_read_single_threaded(state, SHARED, SEQUENTIAL);
   } else {
@@ -154,14 +154,18 @@ BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_SEQUE
   }
 }
 
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_SEQUENTIAL)->ArgsProduct({{10, 100, 1000}, {1, 2, 4, 8, 16, 32}})
+BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_SEQUENTIAL)
+    ->ArgsProduct({{100}, {1, 2, 4, 8, 16, 32}})
     ->UseRealTime();
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_RANDOM)->ArgsProduct({{10, 100, 1000}, {1, 2, 4, 8, 16, 32}})
+BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_RANDOM)
+    ->ArgsProduct({{100}, {1, 2, 4, 8, 16, 32}})
     ->UseRealTime();
 
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_SEQUENTIAL)->ArgsProduct({{10, 100, 1000}, {1, 2, 4, 8, 16, 32}})
+BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_SEQUENTIAL)
+    ->ArgsProduct({{100}, {1, 2, 4, 8, 16, 32}})
     ->UseRealTime();
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_RANDOM)->ArgsProduct({{10, 100, 1000}, {1, 2, 4, 8, 16, 32}})
+BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_RANDOM)
+    ->ArgsProduct({{100}, {1, 2, 4, 8, 16, 32}})
     ->UseRealTime();
 
 }  // namespace hyrise
