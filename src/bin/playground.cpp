9a1287014b49e9d831a4fee9ddd4ec9e982e2248
Jan.Lemcke <janlemcke@live.de>
2022-12-12 10:24:19 +0100

    I think, it is done?!

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -1,9 +1,9 @@
 
-#include <vector>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <vector>
 #include "types.hpp"
 
 using namespace hyrise;  // NOLINT
@@ -28,7 +28,7 @@ void printData(chunk_type data) {
 }
 
 void printVector(std::vector<uint32_t> vector) {
-  std::cout << "Flattend vector:" << std::endl;
+  std::cout << "Vector:" << std::endl;
   for (uint32_t i : vector) {
     std::cout << i << ' ';
   }
@@ -79,9 +79,8 @@ chunk_type createChunk(const uint32_t row_count, const uint32_t column_count) {
   return chunk;
 }
 
-void writeDataToFile(std::vector<uint32_t> flattened_chunk) {
+void writeDataToFile(std::vector<uint32_t> flattened_chunk, const char* filename) {
   const ssize_t NUMBER_OF_BYTES = flattened_chunk.size() * sizeof(uint32_t);
-  const char* filename = "flattened_vector.txt";
   auto fd = int32_t{};
   Assert(((fd = creat(filename, O_WRONLY)) >= 1), fail_and_close_file(fd, "Create error: ", errno));
   chmod(filename, S_IRWXU);  // enables owner to rwx file
@@ -90,20 +89,94 @@ void writeDataToFile(std::vector<uint32_t> flattened_chunk) {
   close(fd);
 }
 
-std::vector<uint32_t> readDataFromFile(){
+std::vector<uint32_t> readDataFromFile(size_t number_of_bytes, const char* filename) {
   auto read_data = std::vector<u_int32_t>{};
-  //TODO(@everyone) Reading steps + evaluation
+  const auto NUMBER_OF_ITEMS = number_of_bytes / sizeof(u_int32_t);
+  read_data.reserve(NUMBER_OF_ITEMS);
+  auto fd = int32_t{};
+  Assert(((fd = open(filename, O_RDONLY)) >= 0), fail_and_close_file(fd, "Open error: ", errno));
+
+  // Getting the mapping to memory.
+  const auto OFFSET = off_t{0};
+  auto* map = reinterpret_cast<u_int32_t*>(mmap(NULL, number_of_bytes, PROT_READ, MAP_PRIVATE, fd, OFFSET));
+  Assert((map != MAP_FAILED), fail_and_close_file(fd, "Mapping Failed: ", errno));
+
+  madvise(map, number_of_bytes, MADV_SEQUENTIAL);
+  for (auto index = size_t{0}; index < NUMBER_OF_ITEMS; ++index) {
+    read_data.push_back(map[index]);
+  }
+
+  // Remove memory mapping after job is done.
+  Assert((munmap(map, number_of_bytes) == 0), fail_and_close_file(fd, "Unmapping failed: ", errno));
   return read_data;
 }
 
+chunk_type recreateChunk(const uint32_t row_count, const uint32_t column_count, std::vector<uint32_t> data_vector) {
+  Assert(row_count % column_count == 0, "Row count is not a multiple of column count!");
+  auto chunk = chunk_type{};
+  const auto VALUE_AMOUNT = data_vector.size();
+
+  std::cout << "We want to recreate one chunk with " << column_count << " columns, " << row_count << " rows and thus "
+            << VALUE_AMOUNT << " values." << std::endl;
+  chunk.reserve(column_count);
+
+  // create columns
+  for (uint32_t i = 0; i < column_count; ++i) {
+    auto new_column = std::vector<uint32_t>{};
+    new_column.reserve(row_count);
+    chunk.push_back(std::make_shared<std::vector<uint32_t>>(new_column));
+  }
+
+  std::cout << chunk.size() << " columns created." << std::endl;
+
+  // create rows / insert values
+  for (size_t i = 0; i < VALUE_AMOUNT; ++i) {
+    auto column_index = i % column_count;
+    auto column = chunk.at(column_index);
+    column->push_back(data_vector.at(i));
+  }
+
+  std::cout << "Values loaded." << std::endl;
+  return chunk;
+}
+
+void sanityCheck(chunk_type original_chunk, chunk_type read_chunk){
+  const size_t ROW_COUNT = original_chunk.size();
+  const size_t COLUMN_COUNT = original_chunk.at(0)->size();
+
+  Assert(ROW_COUNT == read_chunk.size(), "Amount of rows doesn't match!");
+  Assert(COLUMN_COUNT == read_chunk.at(0)->size(), "Amount of columns doesn't match!");
+
+  for (size_t i = 0; i < COLUMN_COUNT; i++) {
+    for (size_t j = 0; j < ROW_COUNT; j++) {
+      auto original_value = original_chunk.at(j)->at(i);
+      auto read_value = read_chunk.at(j)->at(i);
+      Assert(original_value == read_value, "Values are not the same!");
+    }
+  }
+
+}
+
+
 int main() {
   std::cout << "Playground started." << std::endl;
+  const char* filename = "flattened_vector.txt";
   const auto COLUMN_COUNT = uint32_t{5};
   const auto ROW_COUNT = uint32_t{10};
   auto chunk = createChunk(ROW_COUNT, COLUMN_COUNT);
   printData(chunk);
   auto flattened_chunk = flatten(chunk);
+  const ssize_t NUMBER_OF_BYTES = flattened_chunk.size() * sizeof(uint32_t);
   printVector(flattened_chunk);
-  writeDataToFile(flattened_chunk);
+  writeDataToFile(flattened_chunk, filename);
+
+  std::cout << "Finished writing." << std::endl;
+  std::cout << "Start reading." << std::endl;
+
+  auto read_vector = readDataFromFile(NUMBER_OF_BYTES, filename);
+  printVector(read_vector);
+  auto read_chunk = recreateChunk(ROW_COUNT, COLUMN_COUNT, read_vector);
+  printData(read_chunk);
+  sanityCheck(chunk, read_chunk);
   return 0;
 }
