78f7538436b390b1460da2804ae63296a97c27a8
Sandro Speh <sandrospeh@googlemail.com>
2022-12-12 13:51:52 +0100

    Feedback: Renaming, autoing

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -15,22 +15,22 @@ std::string fail_and_close_file(int32_t fd, std::string message, int error_num) 
   return message + std::strerror(error_num);
 }
 
-void printData(chunk_type data) {
+void print_data(chunk_type data) {
   const ssize_t ROW_COUNT = data.size();
   const ssize_t COLUMN_COUNT = data.at(0)->size();
   std::cout << "Data:" << std::endl;
-  for (ssize_t i = 0; i < COLUMN_COUNT; i++) {
-    for (ssize_t j = 0; j < ROW_COUNT; j++) {
-      std::cout << data.at(j)->at(i) << " ";
+  for (auto column_index = ssize_t{0}; column_index < COLUMN_COUNT; column_index++) {
+    for (auto row_index = ssize_t{0}; row_index < ROW_COUNT; row_index++) {
+      std::cout << data.at(row_index)->at(column_index) << " ";
     }
     std::cout << std::endl;
   }
 }
 
-void printVector(std::vector<uint32_t> vector) {
+void print_vector(std::vector<uint32_t> values) {
   std::cout << "Flattend vector:" << std::endl;
-  for (uint32_t i : vector) {
-    std::cout << i << ' ';
+  for (auto value : values) {
+    std::cout << value << ' ';
   }
   std::cout << std::endl;
 }
@@ -39,18 +39,18 @@ std::vector<uint32_t> flatten(chunk_type const& chunk) {
   const ssize_t ROW_COUNT = chunk.size();
   const ssize_t COLUMN_COUNT = chunk.at(0)->size();
 
-  std::vector<uint32_t> flattened;
+  auto flattened = std::vector<uint32_t>();
   flattened.reserve(ROW_COUNT * COLUMN_COUNT);
 
-  for (ssize_t i = 0; i < COLUMN_COUNT; i++) {
-    for (ssize_t j = 0; j < ROW_COUNT; j++) {
-      flattened.push_back(chunk.at(j)->at(i));
+  for (auto column_index = ssize_t{0}; column_index < COLUMN_COUNT; column_index++) {
+    for (auto row_index = ssize_t{0}; row_index < ROW_COUNT; row_index++) {
+      flattened.push_back(chunk.at(row_index)->at(column_index));
     }
   }
   return flattened;
 }
 
-chunk_type createChunk(const uint32_t row_count, const uint32_t column_count) {
+chunk_type create_chunk(const uint32_t row_count, const uint32_t column_count) {
   Assert(row_count % column_count == 0, "Row count is not a multiple of column count!");
   auto chunk = chunk_type{};
   const auto VALUE_AMOUNT = column_count * row_count;
@@ -60,7 +60,7 @@ chunk_type createChunk(const uint32_t row_count, const uint32_t column_count) {
   chunk.reserve(column_count);
 
   // create columns
-  for (uint32_t i = 0; i < column_count; ++i) {
+  for (auto index = ssize_t{0}; index < column_count; ++index) {
     auto new_column = std::vector<uint32_t>{};
     new_column.reserve(row_count);
     chunk.push_back(std::make_shared<std::vector<uint32_t>>(new_column));
@@ -69,18 +69,18 @@ chunk_type createChunk(const uint32_t row_count, const uint32_t column_count) {
   std::cout << chunk.size() << " columns created." << std::endl;
 
   // create rows / insert values
-  for (uint32_t i = 0; i < VALUE_AMOUNT; ++i) {
-    auto column_index = i % column_count;
+  for (auto index = ssize_t{0}; index < VALUE_AMOUNT; ++index) {
+    auto column_index = index % column_count;
     auto column = chunk.at(column_index);
-    column->push_back(i);
+    column->push_back(index);
   }
 
   std::cout << "Values populated." << std::endl;
   return chunk;
 }
 
-void writeDataToFile(std::vector<uint32_t> flattened_chunk) {
-  const ssize_t NUMBER_OF_BYTES = flattened_chunk.size() * sizeof(uint32_t);
+void write_data_to_file(std::vector<uint32_t> flattened_chunk) {
+  const auto NUMBER_OF_BYTES = static_cast<ssize_t>(flattened_chunk.size() * sizeof(uint32_t));
   const char* filename = "flattened_vector.txt";
   auto fd = int32_t{};
   Assert(((fd = creat(filename, O_WRONLY)) >= 1), fail_and_close_file(fd, "Create error: ", errno));
@@ -90,7 +90,7 @@ void writeDataToFile(std::vector<uint32_t> flattened_chunk) {
   close(fd);
 }
 
-std::vector<uint32_t> readDataFromFile(){
+std::vector<uint32_t> read_data_from_file(){
   auto read_data = std::vector<u_int32_t>{};
   //TODO(@everyone) Reading steps + evaluation
   return read_data;
@@ -100,10 +100,10 @@ int main() {
   std::cout << "Playground started." << std::endl;
   const auto COLUMN_COUNT = uint32_t{5};
   const auto ROW_COUNT = uint32_t{10};
-  auto chunk = createChunk(ROW_COUNT, COLUMN_COUNT);
-  printData(chunk);
+  auto chunk = create_chunk(ROW_COUNT, COLUMN_COUNT);
+  print_data(chunk);
   auto flattened_chunk = flatten(chunk);
-  printVector(flattened_chunk);
-  writeDataToFile(flattened_chunk);
+  print_vector(flattened_chunk);
+  write_data_to_file(flattened_chunk);
   return 0;
 }
