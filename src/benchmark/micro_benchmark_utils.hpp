4adb71a7535b2b757a6de3f1dcad69ad988b3d98
Your Name <you@example.com>
2023-01-09 16:18:09 +0000

    Renamed Method

--- src/benchmark/micro_benchmark_utils.hpp
+++ src/benchmark/micro_benchmark_utils.hpp
@@ -12,6 +12,6 @@ void micro_benchmark_clear_disk_cache();
 void aio_error_handling(aiocb* aiocb, uint32_t expected_bytes);
 std::vector<uint32_t> generate_random_indexes(uint32_t number);
 std::vector<uint32_t> generate_random_positive_numbers(uint32_t size);
-std::string fail_and_close_file(int32_t fd, std::string message, int error_num);
+std::string close_file_and_return_error_message(int32_t fd, std::string message, int error_num);
 
 }  // namespace hyrise
