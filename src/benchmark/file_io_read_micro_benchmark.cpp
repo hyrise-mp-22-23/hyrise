52141fd89b33f6d6b49e1df124ef459504d807b8
Jan Lemcke <janlemcke@live.de>
2023-01-13 13:43:58 +0000

    started libaio random helper function

--- src/benchmark/file_io_read_micro_benchmark.cpp
+++ src/benchmark/file_io_read_micro_benchmark.cpp
@@ -91,6 +91,44 @@ void read_data_using_libaio(const size_t thread_from, const size_t thread_to, in
   io_destroy(ctx);
 }
 
+void read_data_randomly_using_libaio(const size_t thread_from, const size_t thread_to, int32_t fd, uint32_t* read_data_start,
+                                     const std::vector<uint32_t>& random_indices) {
+  const auto uint32_t_size = ssize_t{sizeof(uint32_t)};
+  const auto REQUEST_COUNT = uint32_t{64};
+  const auto NUMBER_OF_ELEMENTS_PER_THREAD = (thread_to - thread_from);
+
+  io_context_t ctx;
+  memset(&ctx, 0, sizeof(ctx));
+  io_setup(REQUEST_COUNT, &ctx);
+
+  auto batch_size_thread = static_cast<uint64_t>(std::ceil(static_cast<float>(NUMBER_OF_ELEMENTS_PER_THREAD) / REQUEST_COUNT));
+
+  auto iocbs = std::vector<iocb>(REQUEST_COUNT);
+  auto iocb_list = std::vector<iocb*>(REQUEST_COUNT);
+
+  //TODO adapt code on a way that it uses random_indices
+  for (auto index = size_t{0}; index < REQUEST_COUNT; ++index) {
+    auto from = batch_size_thread * index + thread_from;
+    auto to = from + batch_size_thread;
+    if (to >= NUMBER_OF_ELEMENTS_PER_THREAD) {
+      to = NUMBER_OF_ELEMENTS_PER_THREAD;
+    }
+
+    // io_prep_pread(struct iocb *iocb, int fd, void *buf, size_t count, long long offset);
+    io_prep_pread(&iocbs[index], fd, read_data_start + from, batch_size_thread * uint32_t_size, from * uint32_t_size);
+    iocb_list[index] = &iocbs[index];
+  }
+
+  auto return_value = io_submit(ctx, REQUEST_COUNT, iocb_list.data());
+  Assert(return_value == REQUEST_COUNT, close_file_and_return_error_message(fd, "Asynchronous read using io_submit failed.", return_value));
+
+  auto events = std::vector<io_event>(REQUEST_COUNT);
+  auto events_count = io_getevents(ctx, REQUEST_COUNT, REQUEST_COUNT, events.data(), NULL);
+  Assert(events_count == REQUEST_COUNT, close_file_and_return_error_message(fd, "Asynchronous read using io_getevents failed. ", events_count));
+
+  io_destroy(ctx);
+}
+
 // TODO(everyone): Reduce LOC by making this function more modular (do not repeat it with different function inputs).
 void FileIOMicroReadBenchmarkFixture::read_non_atomic_multi_threaded(benchmark::State& state, uint16_t thread_count) {
   auto filedescriptors = std::vector<int32_t>(thread_count);
