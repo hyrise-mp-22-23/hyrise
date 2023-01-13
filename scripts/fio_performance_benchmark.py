0927efea2d87d88e8ca0d0d4edd09ce922e2e254
phoeinx <hradilak@uni-potsdam.de>
2023-01-13 13:38:35 +0100

    Add multiple io engines to fio benchmark script

--- scripts/fio_performance_benchmark.py
+++ scripts/fio_performance_benchmark.py
@@ -3,10 +3,19 @@ import time
 import subprocess
 
 thread_range = [1, 2, 4, 8, 16, 32, 48, 64]
+io_types = ["read", "randread"]
+filesizes = ["1000M"]
 
-run_types = ['read', 'randread']
-
-filesizes = ['1000M']
+async_io_io_depth = 64
+ioengine_configs = [
+    # ('io_engine', 'additional parameters')
+    ("sync", ""),
+    ("psync", ""),
+    ("mmap", ""),
+    ("io_uring", f"--direct=1 --iodepth={async_io_io_depth}"),
+    ("libaio", f"--direct=1 --iodepth={async_io_io_depth}"),
+    ("posixaio", f"--direct=1 --iodepth={async_io_io_depth}"),
+]
 
 # fio --minimal hardcoded positions
 fio_total_io_pos = 5
@@ -15,8 +24,10 @@ fio_runtime_under_test = 8
 fio_bandwidth_mean = 44
 
 kernel_version = os.uname()[2]
-columns = "name,iterations,real_time,cpu_time,time_unit,bytes_per_second,items_per_second,label,error_occurred," \
-          "error_message"
+columns = (
+    "name,iterations,real_time,cpu_time,time_unit,bytes_per_second,items_per_second,label,error_occurred,"
+    "error_message"
+)
 f = open(f"""fio_benchmark_{kernel_version}_{time.strftime("%H%M%S")}_fio.csv""", "w+")
 f.write(columns + "\n")
 
@@ -28,39 +39,30 @@ def run_and_write_command(run, command, fio_type_offset, fio_size, numjobs):
         fio_type_offset = 41
     # fio is called with --group_reporting. This means that all
     # statistics are group for different jobs.
-    split_output = output.split(b';')
+    split_output = output.split(b";")
     total_io = float(split_output[fio_type_offset + fio_total_io_pos].decode("utf-8"))
     bandwidth = float(split_output[fio_type_offset + fio_bandwidth].decode("utf-8"))
     runtime = float(split_output[fio_type_offset + fio_runtime_under_test].decode("utf-8"))
     bandwidth_mean = float(split_output[fio_type_offset + fio_bandwidth_mean].decode("utf-8"))
-    result = f"\"FileIOMicroBenchmarkFixture/FIO_{run}/{fio_size[:-1]}/{numjobs}/\",1,{str(runtime * 1000)},{str(runtime * 1000)},ns,{str(bandwidth * 1000)},,,,\n"""
+    result = (
+        f'"FileIOMicroBenchmarkFixture/FIO_{run}/{fio_size[:-1]}/{numjobs}/",1,{str(runtime * 1000)},{str(runtime * 1000)},ns,{str(bandwidth * 1000)},,,,\n'
+        ""
+    )
     f.write(result)
     f.flush()
 
 
 for fio_size in filesizes:
-    for run in run_types:
+    for io_type in io_types:
+        for io_engine_config in ioengine_configs:
+            for numjobs in thread_range:
+                if numjobs == 1:
+                    command = f"""sudo fio -minimal -name=fio-bandwidth --bs=4k --size={fio_size} --rw={io_type} --ioengine={io_engine_config[0]} {io_engine_config[1]} --filename=file.txt --group_reporting --refill_buffers"""
+                else:
+                    command = f"""sudo fio -minimal -name=fio-bandwidth --bs=4k --size={fio_size} --rw={io_type} --ioengine={io_engine_config[0]} {io_engine_config[1]} --filename=file.txt --group_reporting --refill_buffers --numjobs={numjobs} --thread"""
 
-        # multithreaded
-        for numjobs in thread_range:
-            command = f"""sudo fio -minimal -name=fio-bandwidth --bs=4k --size={
-            fio_size} --direct=1 --rw={run} --filename=file.txt --numjobs={
-            numjobs} --group_reporting --thread --refill_buffers --ioengine=sync"""
-
-            fio_type_offset = 0
-            print(command)
-            run_and_write_command(run, command, fio_type_offset, fio_size, numjobs)
-
-        # single-threaded
-        numjobs = 1
-
-        command = f"""sudo fio -minimal -name=fio-bandwidth --bs=4k --ioengine=sync --size={
-        fio_size} --direct=1 --rw={
-        run} --filename=file.txt --numjobs={
-        numjobs} --group_reporting --refill_buffers"""
-
-        fio_type_offset = 0
-        print(command)
-        run_and_write_command(run, command, fio_type_offset, fio_size, numjobs)
+                fio_type_offset = 0
+                print(command)
+                run_and_write_command(io_type, command, fio_type_offset, fio_size, numjobs)
 
 f.closed
