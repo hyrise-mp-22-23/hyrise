801a5b7276a3805c38fa86732f1894c720006a3d
phoeinx <hradilak@uni-potsdam.de>
2023-03-12 23:03:42 +0100

    Add benchmark realistic scenario mmap

---
+++ scripts/benchmark_realistic_mmap.py
@@ -0,0 +1,33 @@
+import subprocess
+import itertools
+import math
+
+num_cores = [1, 2, 4, 6, 8, 12, 16, 24, 32, 48, 64]
+for num_core, do_warmup in itertools.product(num_cores, [True, False]):
+    print(num_core, do_warmup)
+    benchmark_command = [
+        'numactl',
+        '-m',
+        '0',
+        '-N',
+        '0',
+        './cmake-build-release/hyriseBenchmarkTPCH',
+        '-s',
+        '10',
+        '--scheduler',
+        f'--clients={num_core}',
+        f'--cores={math.ceil(num_core/2)}',
+        '-m',
+        'Shuffled',
+        '-t',
+        '1200',
+        '-o',
+        f'benchmark_mmap_hyrise_{num_core}_cores_{"with_warmup" if do_warmup else ""}.json'
+    ]
+    if (do_warmup):
+        benchmark_command.append('-w')
+        benchmark_command.append('20')
+
+    print("Executing command: " + subprocess.list2cmdline(benchmark_command) + "\n")
+    p = subprocess.Popen(benchmark_command, stdout=subprocess.PIPE)
+    p.wait()
