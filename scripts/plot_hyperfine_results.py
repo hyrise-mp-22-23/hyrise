3824a7dfd558ce6a01c71fac505cd76c684ab8a7
jan <janlemcke@live.de>
2023-03-13 16:58:39 +0100

    Visualization script for hyperfine.

---
+++ scripts/plot_hyperfine_results.py
@@ -0,0 +1,26 @@
+import json
+import matplotlib.pyplot as plt
+
+# read in the JSON data
+path = '../cmake-build-debug/output.json'
+with open(path, 'r') as f:
+    data = json.load(f)
+
+labels = ['Hyrise Master', 'Hyrise MMAP']
+times = []
+master_mean = data["results"][0]["mean"]
+scale_factor = data["results"][0]["command"].split("-s ")[1].strip()
+
+for result in data["results"]:
+    centered_data = [time / master_mean for time in result["times"]]
+    times.append(centered_data)
+
+# create a boxplot using matplotlib
+fig, ax = plt.subplots()
+ax.boxplot(times, labels=labels)
+
+ax.set_title(f'Comparison of Hyrise Master and MMAP sf={scale_factor}')
+ax.set_xlabel('System')
+ax.set_ylabel('Time (s)')
+
+plt.show()
