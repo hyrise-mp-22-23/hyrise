565cf2daa34dc2c326fb6c3b4f769e5b01c4eaa0
phoeinx <hradilak@uni-potsdam.de>
2023-03-23 12:45:54 +0100

    Fix bug in fixed cores benchmark scripts.

--- scripts/benchmark_hyrise_fixed_multicore.py
+++ scripts/benchmark_hyrise_fixed_multicore.py
@@ -10,8 +10,8 @@ print("Working directory: " + os.getcwd())
 num_cores = [24, 48]
 warmup = [True, False]
 num_clients = [1, 2, 4, 8, 16, 24, 32, 48]
-for num_core, do_warmup in itertools.product(num_cores, num_clients, warmup):
-    print(f"Num cores: {num_core}, Num clients: {num_clients}, Warmup: {do_warmup}")
+for num_core, num_client, do_warmup in itertools.product(num_cores, num_clients, warmup):
+    print(f"Num cores: {num_core}, Num clients: {num_client}, Warmup: {do_warmup}")
     benchmark_command = [
         'numactl',
         '-m',
@@ -22,14 +22,14 @@ for num_core, do_warmup in itertools.product(num_cores, num_clients, warmup):
         '-s',
         '10',
         '--scheduler',
-        f'--clients={num_clients}',
+        f'--clients={num_client}',
         f'--cores={num_core}',
         '-m',
         'Shuffled',
         '-t',
         '1200',
         '-o',
-        f'benchmark_hyrise_master_{num_core}_cores_{num_clients}_clients_{"with_warmup" if do_warmup else ""}.json'
+        f'benchmark_hyrise_master_{num_core}_cores_{num_client}_clients_{"with_warmup" if do_warmup else ""}.json'
     ]
     if (do_warmup):
         benchmark_command.append('-w')
--- scripts/benchmark_realistic_mmap_fixed_multicore.py
+++ scripts/benchmark_realistic_mmap_fixed_multicore.py
@@ -7,9 +7,9 @@ import os
 num_cores = [24, 48]
 warmup = [True, False]
 num_clients = [1, 2, 4, 8, 16, 24, 32, 48]
-for num_core, do_warmup in itertools.product(num_cores, num_clients, warmup):
+for num_core, num_client, do_warmup in itertools.product(num_cores, num_clients, warmup):
     os.system("sudo rm -rf *.bin")
-    print(f'Num Core: {num_core}, NumClients: {num_clients}, Warmup: {do_warmup}')
+    print(f'Num Core: {num_core}, NumClients: {num_client}, Warmup: {do_warmup}')
     benchmark_command = [
         'numactl',
         '-m',
@@ -20,14 +20,14 @@ for num_core, do_warmup in itertools.product(num_cores, num_clients, warmup):
         '-s',
         '10',
         '--scheduler',
-        f'--clients={num_clients}',
+        f'--clients={num_client}',
         f'--cores={num_core}',
         '-m',
         'Shuffled',
         '-t',
         '1200',
         '-o',
-        f'benchmark_mmap_hyrise_{num_core}_cores_{num_clients}_clients_{"with_warmup" if do_warmup else ""}.json'
+        f'benchmark_mmap_hyrise_{num_core}_cores_{num_client}_clients_{"with_warmup" if do_warmup else ""}.json'
     ]
     if (do_warmup):
         benchmark_command.append('-w')
